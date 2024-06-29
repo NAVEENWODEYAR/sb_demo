cfd6885aeb64557c2b358082f843e06480b47550
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-30 01:04:21 +0530

    Mockito for testing

--- src/test/java/com/pivotal/SbStarterApplicationTests.java
+++ src/test/java/com/pivotal/SbStarterApplicationTests.java
@@ -1,9 +1,11 @@
 package com.pivotal;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@InjectMocks
 class SbStarterApplicationTests {
 
 	@Test
